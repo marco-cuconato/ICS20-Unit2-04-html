d66579f013758455ea8f974a5a34451f741fae78
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-03-28 16:51:22 +0000

    add linters

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function enterclicked () {
+  // input
+  const base = parseInt(document.getElementById('base').value)
+  const height = parseInt(document.getElementById('height').value)
+  
+  // process
+  const area = (base * height) / 2
+  
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
